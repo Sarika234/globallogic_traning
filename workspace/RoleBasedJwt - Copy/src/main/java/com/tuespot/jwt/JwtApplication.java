6f785ff4cbb87109122ef61f2abb8020a84ad1ed
ansarpjbd <ansarp.jbd@gmail.com>
2022-08-24 10:06:27 +0530

    rolebased commit

---
+++ workspace/RoleBasedJwt - Copy/src/main/java/com/tuespot/jwt/JwtApplication.java
@@ -0,0 +1,18 @@
+package com.tuespot.jwt;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Ravinder 
+ *
+ */
+
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
